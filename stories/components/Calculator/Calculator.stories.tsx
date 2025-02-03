51834198df99ab71bc36fca36f8a300ba5c2acce
Srinivasan Shanmugam <vasan1983sri@gmail.com>
2025-02-03 06:36:01 -0500

    Calculator updated

--- stories/components/Calculator/Calculator.stories.tsx
+++ stories/components/Calculator/Calculator.stories.tsx
@@ -1,5 +1,7 @@
 import type {Meta, StoryObj} from "@storybook/react";
 import Calculator from "./Calculator";
+import {within, userEvent} from "@storybook/testing-library";
+import {expect} from "@storybook/jest";
 
 const meta: Meta<typeof Calculator> = {
     component: Calculator,
@@ -12,4 +14,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const defaultCalculator: Story = {
+    play: async ({canvasElement}) => {
+        const canvas = within(canvasElement)
+        const button8Exists = canvas.getByText('8')
+        await expect(button8Exists).toBeInTheDocument();
+    }
 }
